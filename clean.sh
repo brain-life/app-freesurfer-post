89e4ebb4f8222073d07ea472bbfa6bfc974f8c37
Brent McPherson <bcmcpher@iu.edu>
2017-07-06 17:18:41 -0400

    add all the files

---
+++ clean.sh
@@ -0,0 +1,15 @@
+rm app-freesurfer-post.e*
+rm app-freesurfer-post.o*
+rm submit.pbs.*
+rm submit.pbs
+rm finished
+rm products.json
+rm jobid
+
+rm -rf t1
+
+rm *.pial
+rm *.vtk
+rm -rf output
+
+rm fsaverage
